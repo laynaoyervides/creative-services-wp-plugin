adee145164e7663b7dd7d6b03bb09d622344bbe4
Layna Oyervides <laynaoyervides@gmail.com>
2023-03-21 21:34:48 -0600

    initial

---
+++ wordpress/wp-content/plugins/creative-services/creative-services.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name:  Creative Services Sample App
+Description:  Sample App for Assessment
+Plugin URI:   https://profiles.wordpress.org/
+Author:       Layna Oyervides
+Version:      1.0
+Text Domain:  creativeservices
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
+
+
